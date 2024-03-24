7e8a4ac1e8461a091735e90731c131027ac4d857
jjlim7 <jj.limmm159@gmail.com>
2024-03-24 23:52:16 +0800

    Refactor get and update points

--- functions/points/update-points/main.go
+++ functions/points/update-points/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/vynious/ascenda-lp-backend/db"
+	"github.com/vynious/ascenda-lp-backend/types"
+)
+
+var (
+	DB  *db.DBService
+	err error
+)
+
+func init() {
+	log.Printf("INIT")
+	DB, err = db.SpawnDBService()
+}
+
+func main() {
+	// we are simulating a lambda behind an ApiGatewayV2
+	lambda.Start(handler)
+
+	defer DB.CloseConn()
+}
+
+func handler(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	req := types.UpdatePointsRequestBody{}
+	if err := json.Unmarshal([]byte(request.Body), &req); err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 400,
+			Body:       errors.New("invalid request. malformed request found").Error(),
+		}, nil
+	}
+
+	if req.ID == nil || req.NewBalance == nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 400,
+			Body:       errors.New("bad request. id or new_balance not found").Error(),
+		}, nil
+	}
+	log.Printf("UpdatePoints %s", *req.ID)
+
+	pointsRecord, err := DB.UpdatePoints(ctx, *req.ID, *req.NewBalance)
+	if pointsRecord == nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 400,
+			Body:       err.Error(),
+		}, nil
+	}
+
+	obj, _ := json.Marshal(pointsRecord)
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: 200,
+		Body:       string(obj),
+	}, nil
+}
