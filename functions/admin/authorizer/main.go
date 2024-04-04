3f6b4facef91ed29dc3bfcbd2b52a4595b787c7a
vynious <shawnthiah@gmail.com>
2024-04-04 23:13:24 +0800

    cognito can get role from token

--- functions/admin/authorizer/main.go
+++ functions/admin/authorizer/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/uuid"
 	"github.com/vynious/ascenda-lp-backend/db"
 	"github.com/vynious/ascenda-lp-backend/types"
 	"github.com/vynious/ascenda-lp-backend/util"
@@ -29,6 +30,7 @@ func init() {
 
 func AuthorizerHandler(ctx context.Context, req events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 	token := req.AuthorizationToken
+
 	roleName, err := util.GetRoleWithCognito(token)
 	if err != nil {
 
@@ -41,25 +43,48 @@ func AuthorizerHandler(ctx context.Context, req events.APIGatewayCustomAuthorize
 	var permissions types.RolePermissionList
 	permissions = role.Permissions
 
+	return generatePolicy(permissions, uuid.NewString(), req.MethodArn), nil
 }
 
-func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+func generatePolicy(permissions []types.RolePermission, principalId, resource string) events.APIGatewayCustomAuthorizerResponse {
 	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
 
-	if effect != "" && resource != "" {
-		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{
-				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   effect,
-					Resource: []string{resource},
-				},
-			},
+	authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+		Version:   "2012-10-17",
+		Statement: []events.IAMPolicyStatement{},
+	}
+
+	for _, permission := range permissions {
+		// For each permission, check if the operation is allowed and add the corresponding policy statement
+		if permission.Resource == resource {
+			if permission.CanRead {
+				statement := generateStatement("execute-api:Invoke", "Allow", resource)
+				authResponse.PolicyDocument.Statement = append(authResponse.PolicyDocument.Statement, statement)
+			}
+			if permission.CanCreate {
+				statement := generateStatement("execute-api:Invoke", "Allow", resource)
+				authResponse.PolicyDocument.Statement = append(authResponse.PolicyDocument.Statement, statement)
+			}
+			if permission.CanUpdate {
+				statement := generateStatement("execute-api:Invoke", "Allow", resource)
+				authResponse.PolicyDocument.Statement = append(authResponse.PolicyDocument.Statement, statement)
+			}
+			if permission.CanDelete {
+				statement := generateStatement("execute-api:Invoke", "Allow", resource)
+				authResponse.PolicyDocument.Statement = append(authResponse.PolicyDocument.Statement, statement)
+			}
 		}
 	}
 
 	return authResponse
+}
+
+func generateStatement(action, effect, resource string) events.IAMPolicyStatement {
+	return events.IAMPolicyStatement{
+		Action:   []string{action},
+		Effect:   effect,
+		Resource: []string{resource},
+	}
 }
 
 func main() {
